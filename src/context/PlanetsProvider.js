ad35d9cef76f0277ad16f95087cf48647e772b79
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-05-02 15:27:22 -0300

    Requisito 1

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,9 +1,29 @@
 import PropTypes from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 import PlanetsContext from './PlanetsContext';
 
 function PlanetsProvider({ children }) {
+  const [apiResult, setApiResult] = useState([]);
+  const fetchApiPlanets = async () => {
+    try {
+      const response = await fetch('https://swapi.dev/api/planets');
+      const data = await response.json();
+      setApiResult(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiPlanets();
+  }, []);
+
+  const context = useMemo(() => ({
+    apiResult,
+  }), [apiResult]);
+
   return (
-    <PlanetsContext.Provider value="natália">
+    <PlanetsContext.Provider value={ context }>
       {children}
     </PlanetsContext.Provider>
   );
