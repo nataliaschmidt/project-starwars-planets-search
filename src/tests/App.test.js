f39b93b7b07e4037550920b8a8180bcb7282b639
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-05-03 11:00:14 -0300

    requisito 10

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,7 +8,7 @@ import { act } from 'react-dom/test-utils';
 
 describe('Testa a aplicação', () => {
   beforeEach(() => {
-      global.fetch = jest.fn().mockResolvedValue({
+    global.fetch = jest.fn().mockResolvedValue({
       json: jest.fn().mockResolvedValue(mockData),
     });
   });
@@ -29,14 +29,14 @@ describe('Testa a aplicação', () => {
     expect(screen.getByTestId('column-sort-input-asc')).toBeInTheDocument();
     expect(screen.getByTestId('column-sort-input-desc')).toBeInTheDocument();
     expect(screen.getByTestId('column-sort-button')).toBeInTheDocument();
-  
+
     await waitFor(() => {
       expect(screen.getByRole('cell', { name: /tatooine/i })).toBeInTheDocument();
       expect(screen.getByRole('cell', { name: /arid/i })).toBeInTheDocument();
     })
   });
 
-  test('Verifica a funcionalidade dos filtros', async() => {
+  test('Verifica a funcionalidade dos filtros', async () => {
     render(
       <PlanetsProvider>
         <App />
@@ -104,7 +104,25 @@ describe('Testa a aplicação', () => {
       userEvent.click(btnFilter);
     })
     const planets4 = screen.getAllByTestId('planet-name');
-    expect(planets1.length).toEqual(2);    
+    expect(planets4.length).toEqual(3);
+
+    userEvent.selectOptions(selectColumn, 'rotation_period');
+    userEvent.selectOptions(selectComparison, 'igual a');
+    userEvent.clear(selectValue)
+    userEvent.type(selectValue, '18');
+    act(() => {
+      userEvent.click(btnFilter);
+    })
+    const planets5 = screen.getAllByTestId('planet-name');
+    expect(planets5.length).toEqual(1);
+
+    const btnDeleteFilter1 = screen.getAllByRole('button', { name: /delete/i })
+    act(() => {
+      userEvent.click(btnDeleteFilter1[1])
+    })
+    const planets6 = screen.getAllByTestId('planet-name');
+    expect(planets6.length).toEqual(3);
+
 
     const sortColumn = screen.getByTestId('column-sort');
     const sortAsc = screen.getByTestId('column-sort-input-asc');
@@ -120,5 +138,49 @@ describe('Testa a aplicação', () => {
     act(() => {
       userEvent.click(btnSort);
     })
+  })
+
+  test('Testa se ao adicionar todas opções de columns o select fica vazio', async () => {
+    render(
+      <PlanetsProvider>
+        <App />
+      </PlanetsProvider>
+    );
+    const selectColumn = screen.getByTestId('column-filter');
+    const selectComparison = screen.getByTestId('comparison-filter');
+    const selectValue = screen.getByTestId('value-filter');
+    const btnFilter = screen.getByTestId('button-filter');
+
+    await waitFor(() => {
+      const planets = screen.getAllByTestId('planet-name');
+      expect(planets.length).toEqual(10);
+    })
+
+    userEvent.selectOptions(selectColumn, 'diameter');
+    userEvent.selectOptions(selectComparison, 'menor que');
+    userEvent.clear(selectValue)
+    userEvent.type(selectValue, '10000');
+    act(() => {
+      userEvent.click(btnFilter);
+    })
+    const planets1 = screen.getAllByTestId('planet-name');
+    expect(planets1.length).toEqual(3);
+
+    userEvent.selectOptions(selectColumn, 'rotation_period');
+    userEvent.selectOptions(selectComparison, 'igual a');
+    userEvent.clear(selectValue)
+    userEvent.type(selectValue, '18');
+    act(() => {
+      userEvent.click(btnFilter);
+    })
+    const planets2 = screen.getAllByTestId('planet-name');
+    expect(planets2.length).toEqual(1);
+
+    const btnDeleteFilter1 = screen.getAllByRole('button', { name: /delete/i })
+    act(() => {
+      userEvent.click(btnDeleteFilter1[0])
+    })
+    const planets3 = screen.getAllByTestId('planet-name');
+    expect(planets3.length).toEqual(1);
   })
 })
